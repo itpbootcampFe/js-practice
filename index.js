d68e25d7fb55424f046780fad9cf60f93e228287
Mark Gun <58705522+mgunsd@users.noreply.github.com>
2021-02-16 21:18:11 +0000

    calback example

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//functions as Values
+
 function topla(x,y) {
   return x+y;
 };
@@ -20,14 +22,47 @@ function bol(x,y) {
 // };
 
 
-const oparations = [topla,fark,carp,bol]
+// const oparations = [topla,fark,carp,bol]
 
-for (islem of oparations ){
-   console.log(islem(5,5));
-}
+// for (islem of oparations ){
+//    console.log(islem(5,5));
+// }
 
+// for (let islem=0; islem<oparations.length; islem++ ){
+//   console.log(oparations[islem](5,5));
+
+// }
 
 // const math = {
 //   plus: topla,
 //   minus: fark,  
 // }
+
+
+//functions as arguments;
+
+function mean (t,b,a,c){
+   return b(t(a,c),2);
+}
+
+const result = mean(topla,bol,8,12);
+
+console.log(`result: `, result);
+
+// console.log ( mean(topla(5,4),bol));
+
+///Calback to call afunction inside a function
+
+function say() {
+  alert("bugunluk bukadar!")
+}
+
+say();
+
+
+setTimeout( function(){
+  alert("bye bye")
+}, 5000
+);
+
+
